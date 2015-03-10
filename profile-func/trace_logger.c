346563e9dbac05b1f8c657ced082acbdc114550b
Sam Xi <samxi@seas.harvard.edu>
2015-03-10 10:35:40 -0400

    Dump gzipped trace files instead of uncompressed trace files.
    This will require all scripts using the tracer to be updated. Also, users will
    need to pull the latest Aladdin changes to get the necessary changes in the DDDG
    builder.

--- profile-func/trace_logger.c
+++ profile-func/trace_logger.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
+#include <zlib.h>
 #define RESULT_LINE 19134
 #define FORWARD_LINE 24601
 
@@ -12,15 +13,15 @@ void trace_logger_log0(int line_number, char *name, char *bbid, char *instid, in
 void trace_logger_log_label();
 void trace_logger_fin();
 
-FILE *full_trace_file;
+gzFile *full_trace_file;
 int initp=0;
 
 int inst_count = 0;
 void trace_logger_init()
 {
-  full_trace_file=fopen("dynamic_trace","w");
+  full_trace_file=gzopen("dynamic_trace.gz","w");
 
-  if (full_trace_file==NULL){ 
+  if (full_trace_file==NULL){
     perror("Failed to open logfile \"dynamic_trace\"");
     exit(-1);
   }
@@ -31,7 +32,7 @@ void trace_logger_fin()
 {
   int i;
 
-  fclose(full_trace_file);
+  gzclose(full_trace_file);
 }
 
 void trace_logger_log0(int line_number, char *name, char *bbid, char *instid, int opcode)
@@ -40,49 +41,49 @@ void trace_logger_log0(int line_number, char *name, char *bbid, char *instid, in
     trace_logger_init();
     initp=1;
   }
-  fprintf(full_trace_file, "\n0,%d,%s,%s,%s,%d,%d\n", line_number, name, bbid, instid, opcode,inst_count);
+  gzprintf(full_trace_file, "\n0,%d,%s,%s,%s,%d,%d\n", line_number, name, bbid, instid, opcode,inst_count);
   inst_count++;
 }
 
 void trace_logger_log_int(int line, int size, int64_t value, int is_reg, char *label)
 {
   assert(initp == 1);
-	if(line==RESULT_LINE)	
-    fprintf(full_trace_file, "r,%d,%ld,%d,%s\n", size, value, is_reg, label);
-	else if(line==FORWARD_LINE)	
-    fprintf(full_trace_file, "f,%d,%ld,%d,%s\n", size, value, is_reg, label);
+	if(line==RESULT_LINE)
+    gzprintf(full_trace_file, "r,%d,%ld,%d,%s\n", size, value, is_reg, label);
+	else if(line==FORWARD_LINE)
+    gzprintf(full_trace_file, "f,%d,%ld,%d,%s\n", size, value, is_reg, label);
 	else
-  	fprintf(full_trace_file, "%d,%d,%ld,%d,%s\n", line, size, value, is_reg, label);
+  	gzprintf(full_trace_file, "%d,%d,%ld,%d,%s\n", line, size, value, is_reg, label);
 }
 void trace_logger_log_double(int line, int size, double value, int is_reg, char *label)
 {
   assert(initp == 1);
-	if(line==RESULT_LINE) 
-    fprintf(full_trace_file, "r,%d,%f,%d,%s\n", size, value, is_reg, label);
-	else if(line==FORWARD_LINE) 
-    fprintf(full_trace_file, "f,%d,%f,%d,%s\n", size, value, is_reg, label);
+	if(line==RESULT_LINE)
+    gzprintf(full_trace_file, "r,%d,%f,%d,%s\n", size, value, is_reg, label);
+	else if(line==FORWARD_LINE)
+    gzprintf(full_trace_file, "f,%d,%f,%d,%s\n", size, value, is_reg, label);
   else
-	  fprintf(full_trace_file, "%d,%d,%f,%d,%s\n",line, size, value, is_reg, label);
+	  gzprintf(full_trace_file, "%d,%d,%f,%d,%s\n",line, size, value, is_reg, label);
 }
 
 void trace_logger_log_int_noreg(int line, int size, int64_t value, int is_reg)
 {
   assert(initp == 1);
-	if(line==RESULT_LINE)	
-    fprintf(full_trace_file, "r,%d,%ld,%d\n", size, value, is_reg);
-	else if(line==FORWARD_LINE)	
-    fprintf(full_trace_file, "f,%d,%ld,%d\n", size, value, is_reg);
+	if(line==RESULT_LINE)
+    gzprintf(full_trace_file, "r,%d,%ld,%d\n", size, value, is_reg);
+	else if(line==FORWARD_LINE)
+    gzprintf(full_trace_file, "f,%d,%ld,%d\n", size, value, is_reg);
 	else
-  	fprintf(full_trace_file, "%d,%d,%ld,%d\n", line, size, value, is_reg);
+  	gzprintf(full_trace_file, "%d,%d,%ld,%d\n", line, size, value, is_reg);
 }
 void trace_logger_log_double_noreg(int line, int size, double value, int is_reg)
 {
   assert(initp == 1);
-	if(line==RESULT_LINE) 
-    fprintf(full_trace_file, "r,%d,%f,%d\n", size, value, is_reg);
-	else if(line==FORWARD_LINE) 
-    fprintf(full_trace_file, "f,%d,%f,%d\n", size, value, is_reg);
+	if(line==RESULT_LINE)
+    gzprintf(full_trace_file, "r,%d,%f,%d\n", size, value, is_reg);
+	else if(line==FORWARD_LINE)
+    gzprintf(full_trace_file, "f,%d,%f,%d\n", size, value, is_reg);
   else
-	  fprintf(full_trace_file, "%d,%d,%f,%d\n",line, size, value, is_reg);
+	  gzprintf(full_trace_file, "%d,%d,%f,%d\n",line, size, value, is_reg);
 }
 
